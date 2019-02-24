85503b7cf01121fba6e70569d6269fb74fab4056
Dat Doan <datdoandanh@gmail.com>
2019-02-24 15:42:34 -0800

    lab 2 - lab - add subject and observers

--- lab02/lab/BankProjectV10/src/main/java/bank/dao/AccountDAO.java
+++ lab02/lab/BankProjectV10/src/main/java/bank/dao/AccountDAO.java
@@ -6,7 +6,7 @@ import bank.domain.Account;
 public class AccountDAO implements IAccountDAO {
 	Collection<Account> accountlist = new ArrayList<Account>();
 
-	public void saveAccount(Account account) {
+		public void saveAccount(Account account) {
 		accountlist.add(account); // add the new
 	}
 
