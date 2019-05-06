a381f6915b9e120afb124e70d9aaee62aa834323
Dat Doan <datdoandanh@gmail.com>
2019-05-05 20:01:16 -0700

    lab 11 - Spring DI

---
+++ lab11_Spring_DI_lab/springexercises/given applications/ProductApplication/src/products/InventoryService.java
@@ -0,0 +1,9 @@
+package products;
+
+public class InventoryService implements IInventoryService {
+
+    @Override
+    public int getNumberInStock(int productNumber) {
+        return productNumber - 200;
+    }
+}
